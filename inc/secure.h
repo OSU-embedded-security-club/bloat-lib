1a0145cd851ed4ef1b8d1b7e5c04e5f1b19280e7
Bonono63 <bonoria63@gmail.com>
2024-11-17 18:03:08 -0500

    m

---
+++ inc/secure.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdint.h>
+
+/*
+ * Initializes ascon and misc requirements for the library
+ * */
+int bloat_initialize(void);
+
+/*
+ * Encrypts the given buffer
+ *
+ * @return whether the encryption was successful
+ * */
+int encrypt(const uint8_t *const key, uint8_t *in_buf, size_t buf_size, uint8_t *out_buf);
+
+/*
+ * Decrypts the given buffer
+ *
+ * @return whether the decryption was successful
+ * */
+int decrypt(uint8_t *out_buf);
---
+++ src/secure.c
@@ -0,0 +1,18 @@
+#include <secure.h>
+
+int bloat_initialize(void)
+{
+        int result = 0;
+
+        return result;
+}
+
+int encrypt(const uint8t *const key, uint8_t *in_buf, size_t buf_size, uint8_t *out_buf)
+{
+        return 0;
+}
+
+int decrypt()
+{
+        return 0;
+}
