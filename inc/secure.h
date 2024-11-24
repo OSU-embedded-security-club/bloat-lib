1d630f9d63c8b0da9b609cc7d58f4fd504614b3b
Mattwike <139834501+Mattwike@users.noreply.github.com>
2024-11-24 17:14:23 -0500

    fixed encrypt

--- inc/secure.h
+++ inc/secure.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stddef.h>
 #include <stdint.h>
+#include "../ascon-c-1.3.0/crypto_aead/ascon128abi32v13/bi32/ascon.h"
 
 /*
  * Initializes ascon and misc requirements for the library
@@ -12,11 +13,10 @@ int bloat_initialize(void);
  *
  * @return whether the encryption was successful
  * */
-int encrypt(const uint8_t *const key, uint8_t *in_buf, size_t buf_size, uint8_t *out_buf);
-
+int encrypt(uint8_t *tag, uint8_t *out_buf, const uint8_t *message, uint64_t mlen, const uint8_t *ad, uint64_t ad_len, const uint8_t *nucance, const uint8_t *key);
 /*
  * Decrypts the given buffer
  *
  * @return whether the decryption was successful
  * */
-int decrypt(uint8_t *out_buf);
+int decrypt(uint8_t *message, const uint8_t *tag, const uint8_t *out_buff, uint64_t buff_len, const uint8_t *ad, uint64_t ad_len, const uint8_t *nuance, const uint8_t *key);
--- src/secure.c
+++ src/secure.c
@@ -1,6 +1,5 @@
 #include <stddef.h>
 #include <secure.h>
-#include <ascon.h>
 
 int bloat_initialize(void)
 {
@@ -9,12 +8,12 @@ int bloat_initialize(void)
         return result;
 }
 
-int encrypt(uint8_t tag, uint8_t out_buf, const uint8_t message, uint64_t mlen, const uint8_t ad, uint64_t ad_len, const uint8_t nucance, const uint8_t key)
+int encrypt(uint8_t *tag, uint8_t *out_buf, const uint8_t *message, uint64_t mlen, const uint8_t *ad, uint64_t ad_len, const uint8_t *nucance, const uint8_t *key)
 {
         return ascon_aead_encrypt(tag, out_buf, message, mlen, ad, ad_len, nucance, key);
 }
 
-int decrypt(uint8_t message, const uint8_t tag, const uint8_t out_buff, uint64_t buff_len, const uint8_t ad, uint64_t ad_len, const uint8_t nuance, const uint8_t key)
+int decrypt(uint8_t *message, const uint8_t *tag, const uint8_t *out_buff, uint64_t buff_len, const uint8_t *ad, uint64_t ad_len, const uint8_t *nuance, const uint8_t *key)
 {
         return ascon_aead_decrypt(message, tag, out_buff, buff_len, ad, ad_len, nuance, key);
 }
